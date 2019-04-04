c6ef3071de977db57cf5cc0ef113b01b088a3396
Mike Wright <mikegordonwright@gmail.com>
2019-04-04 16:46:42 -0400

    link buttons

--- app/src/main/java/com/example/artifact/MainActivity.java
+++ app/src/main/java/com/example/artifact/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.artifact;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected Button button1;
+    protected Button button2;
+    protected Button button3;
+    protected Button button4;
+    protected Button button5;
+    protected Button button6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button1.findViewById(R.id.button1);
+        button2.findViewById(R.id.button2);
+        button3.findViewById(R.id.button3);
+        button4.findViewById(R.id.button4);
+        button5.findViewById(R.id.button5);
+        button6.findViewById(R.id.button6);
+
     }
 }
