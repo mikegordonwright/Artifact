27ce96b335d67b4701b0898df0b628fcbe5c1f99
Mike Wright <mikegordonwright@gmail.com>
2019-04-08 13:30:50 -0400

    language switch fix

--- app/src/main/java/com/example/artifact/MainActivity.java
+++ app/src/main/java/com/example/artifact/MainActivity.java
@@ -8,7 +8,6 @@ import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -25,7 +24,13 @@ public class MainActivity extends AppCompatActivity {
     protected MediaPlayer mp3;
     protected MediaPlayer mp4;
     protected MediaPlayer mp5;
+    protected MediaPlayer mp6;
+    protected MediaPlayer mp7;
+    protected MediaPlayer mp8;
+    protected MediaPlayer mp9;
+    protected MediaPlayer mp10;
     protected Switch switch1;
+    protected TextView textView;
     protected ConstraintLayout conLayout;
 
 
@@ -44,7 +49,10 @@ public class MainActivity extends AppCompatActivity {
         conLayout.setLayoutParams(lp);
         lp.addRule(RelativeLayout.CENTER_IN_PARENT);
 
-        //setup layout elements
+        textView = findViewById(R.id.textView);
+        textView.setText("Honk");
+
+                //setup layout elements
         switch1 = findViewById(R.id.switch1);
         button1 = findViewById(R.id.button1);
         button2 = findViewById(R.id.button2);
@@ -53,16 +61,24 @@ public class MainActivity extends AppCompatActivity {
         button5 = findViewById(R.id.button5);
         button1.setText("Horn");
         button2.setText("Excuse me");
-        button3.setText("lane change");
+        button3.setText("sorry");
         button4.setText("Hello");
-        button5.setText("move!");
+        button5.setText("move");
 
-        //setup sounds
+        //setup sounds english
         mp1 = MediaPlayer.create(this, R.raw.horn);
         mp2 = MediaPlayer.create(this, R.raw.excuseme);
-        mp3 = MediaPlayer.create(this, R.raw.bell);
+        mp3 = MediaPlayer.create(this, R.raw.sorry);
         mp4 = MediaPlayer.create(this, R.raw.hello);
-        mp5 = MediaPlayer.create(this, R.raw.chime);
+        mp5 = MediaPlayer.create(this, R.raw.bell);
+
+
+        //setup sounds french
+        mp7 = MediaPlayer.create(this, R.raw.scuzermoi);
+        mp8 = MediaPlayer.create(this, R.raw.desoler);
+        mp9 = MediaPlayer.create(this, R.raw.allo);
+
+
 
         //all the onclicks
         setupListeners();
@@ -78,16 +94,90 @@ public class MainActivity extends AppCompatActivity {
                 if (switch1.isChecked()) {
                     button1.setText("klaxon");
                     button2.setText("excusez-moi");
-                    button3.setText("changer de voie");
+                    button3.setText("désolé");
                     button4.setText("allo");
-                    button5.setText("bougez!");
-                }else
-                {
+                    button5.setText("bougez");
+                    textView.setText("Klaxon");
+
+                    button1.setOnClickListener(new Button.OnClickListener(){
+                        @Override
+                        public void onClick(View v) {
+                            mp1.start();
+                        }
+                    });
+
+                    button2.setOnClickListener(new Button.OnClickListener(){
+                        @Override
+                        public void onClick(View v) {
+                            mp7.start();
+                        }
+                    });
+
+                    button3.setOnClickListener(new Button.OnClickListener(){
+                        @Override
+                        public void onClick(View v) {
+                            mp8.start();
+                        }
+                    });
+
+                    button4.setOnClickListener(new Button.OnClickListener(){
+                        @Override
+                        public void onClick(View v) {
+                            mp9.start();
+                        }
+                    });
+
+                    button5.setOnClickListener(new Button.OnClickListener(){
+                        @Override
+                        public void onClick(View v) {
+                            mp5.start();
+                        }
+                    });
+
+                } else {
                     button1.setText("Horn");
                     button2.setText("Excuse me");
-                    button3.setText("lane change");
+                    button3.setText("sorry");
                     button4.setText("Hello");
-                    button5.setText("move!");
+                    button5.setText("move");
+                    textView.setText("Honk");
+
+                    button1.setOnClickListener(new Button.OnClickListener(){
+                        @Override
+                        public void onClick(View v) {
+                            mp1.start();
+                        }
+                    });
+
+                    button2.setOnClickListener(new Button.OnClickListener(){
+                        @Override
+                        public void onClick(View v) {
+                            mp2.start();
+                        }
+                    });
+
+                    button3.setOnClickListener(new Button.OnClickListener(){
+                        @Override
+                        public void onClick(View v) {
+                            mp3.start();
+                        }
+                    });
+
+                    button4.setOnClickListener(new Button.OnClickListener(){
+                        @Override
+                        public void onClick(View v) {
+                            mp4.start();
+                        }
+                    });
+
+                    button5.setOnClickListener(new Button.OnClickListener(){
+                        @Override
+                        public void onClick(View v) {
+                            mp5.start();
+                        }
+                    });
+
+
                 }
             }
         });
@@ -110,7 +200,7 @@ public class MainActivity extends AppCompatActivity {
         button3.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View v) {
-                if (!mp1.isPlaying()) mp3.start();
+                mp3.start();
             }
         });
 
@@ -127,6 +217,7 @@ public class MainActivity extends AppCompatActivity {
                 mp5.start();
             }
         });
+
 
 
     }
